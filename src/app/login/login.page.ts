b04db6f91d267ea2f2d6c1c3463d91091089d347
giovaneMiranda <52455546+giovaneMiranda@users.noreply.github.com>
2019-07-13 14:12:41 -0300

    Cadastro - implementação

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../interfaces/user';
+import { LoadingController, ToastController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public userLogin: User = {};
+  public userRegister: User = {};
+  private loading: any;
 
-  constructor() { }
+  constructor(
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private authService: AuthService
+
+  ) { }
 
   ngOnInit() {
   }
 
+  login() {
+
+  }
+
+  async register() {
+    await this.presentLoading();
+    try {
+      await this.authService.register(this.userRegister);
+    } catch (error) {
+      let message: string;
+
+      switch(error.code){
+        case 'auth/weak-password':
+          message= 'Senha deve ter no mínimo 6 caracteres.'
+          break;
+
+          case 'auth/email-already-in-use':
+          message= 'O endereço de e-mail já está sendo usado por outra conta'
+          break;
+
+          case 'auth/invalid-email':
+          message= 'O endereço de email está mal formatado.'
+          break;
+
+          case 'auth/argument-error':
+          message= 'O campo de email e senha devem ser preencidos.'
+          break;
+      }
+      
+      console.error(error);
+
+
+      this.presentToast(message);
+    } finally {
+      this.loading.dismiss();
+    }
+  }
+
+  async presentLoading() {
+    this.loading = await this.loadingController.create({
+      message: 'Carregando...',
+    });
+    return this.loading.present();
+  }
+
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  fireBaseConfig: {
+    apiKey: "AIzaSyDpVrNB-fp_g9Xn683roaMP2iwOLVqvTeM",
+    authDomain: "appsimu-67337.firebaseapp.com",
+    databaseURL: "https://appsimu-67337.firebaseio.com",
+    projectId: "appsimu-67337",
+    storageBucket: "",
+    messagingSenderId: "452912783883",
+    appId: "1:452912783883:web:73b9bcb56419bbc3"
+  }
 };
 
 /*
