abdf74839a2f9ab87af95efb2f90efad992a279a
Ana Carolina Bolsoni <anabolsoni2@hotmail.com>
2019-09-29 12:33:31 -0300

    Alteração Ana

--- src/app/detalhe-acao/detalhe-acao.module.ts
+++ src/app/detalhe-acao/detalhe-acao.module.ts
@@ -5,12 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
-import { AtivosPage } from './ativos.page';
+import { DetalheAcaoPage } from './detalhe-acao.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: AtivosPage
+    component: DetalheAcaoPage
   }
 ];
 
@@ -21,6 +21,6 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [AtivosPage]
+  declarations: [DetalheAcaoPage]
 })
-export class AtivosPageModule {}
+export class DetalheAcaoPageModule {}
