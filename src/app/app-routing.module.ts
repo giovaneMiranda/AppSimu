73e515d65de9497ba3c8e8cc7491468135a1d429
Ana Carolina Bolsoni <anabolsoni2@hotmail.com>
2019-09-29 11:00:37 -0300

    Atualização Ana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { LoginGuard } from './guards/login.guard';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule', canActivate: [AuthGuard] },
-  { path: 'login', loadChildren: './login/login.module#LoginPageModule',canActivate: [LoginGuard]},
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule',canActivate: [LoginGuard]},  { path: 'ativos', loadChildren: './ativos/ativos.module#AtivosPageModule' },
+
 ];
 
 @NgModule({
---
+++ src/app/ativos/ativos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ativos',
+  templateUrl: './ativos.page.html',
+  styleUrls: ['./ativos.page.scss'],
+})
+export class AtivosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
