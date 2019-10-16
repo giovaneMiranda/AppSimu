c9923ddb850773061982545ce42786be5a2fd224
Ana Carolina Bolsoni <anabolsoni2@hotmail.com>
2019-10-15 22:50:47 -0300

    detacao_v7

--- src/app/detalheacao/detalheacao.page.ts
+++ src/app/detalheacao/detalheacao.page.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./detalheacao.page.scss'],
 })
 export class DetalheAcaoPage implements OnInit {
+  comp: boolean;
+  vend: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+ compra() {
+      if (this.comp == true){
+        this.comp = false;
+      }else{
+        this.comp = true;
+        this.vend = false;
+      }
+  }
+
+  venda() {
+    if (this.vend == true){
+      this.vend = false;
+    }else{
+      this.comp = false;
+      this.vend = true;
+    }
   }
 
 }
