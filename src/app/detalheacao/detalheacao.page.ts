490caad1f73c3b24f5e96ec5e35ff0b9d908e6f6
Ana Carolina Bolsoni <anabolsoni2@hotmail.com>
2019-10-25 22:03:39 -0300

    detacao - v_08

--- src/app/detalheacao/detalheacao.page.ts
+++ src/app/detalheacao/detalheacao.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Userbd } from '../interfaces/userbd';
+import { AlertController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-detalheacao',
@@ -8,8 +14,22 @@ import { Component, OnInit } from '@angular/core';
 export class DetalheAcaoPage implements OnInit {
   comp: boolean;
   vend: boolean;
+  public dataUser: Userbd;
 
-  constructor() { }
+  constructor(private authService: AuthService, 
+              private afs: AngularFirestore,
+              private alertController: AlertController) { 
+    this.authService.getAuth().onAuthStateChanged(user => {
+      if (user) {
+        this.afs.collection('User')
+          .doc(user.uid)
+          .valueChanges()
+          .subscribe(docUser => {
+            this.savemoney(docUser)
+          });
+        }
+      });
+  }
 
   ngOnInit() {
   }
@@ -29,7 +49,54 @@ export class DetalheAcaoPage implements OnInit {
     }else{
       this.comp = false;
       this.vend = true;
+
+
+
+
+
+
     }
+  }
+
+  savemoney(doc) {
+    this.dataUser = doc;
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Message <strong>text</strong>!!!',
+      inputs: [
+        {
+          name: 'username',
+          placeholder: 'Username'
+        },
+        {
+          name: 'password',
+          placeholder: 'Password',
+          type: 'password'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+    let result = await alert.onDidDismiss();
+    console.log(result);
   }
 
 }
