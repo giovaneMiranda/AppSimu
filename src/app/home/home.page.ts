5c4e36d723866c1bf3511d27e6fbd454bfedea25
Regina Nalegaca <52335156+renalegaca@users.noreply.github.com>
2019-10-27 17:02:56 -0300

    Home - Atulização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
-import { MenuController } from '@ionic/angular';
+import { MenuController, ModalController } from '@ionic/angular';
+import { Userbd } from '../interfaces/userbd';
+import { OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  comp: boolean;
+  vend: boolean;
+  public dataUser: Userbd;
 
-  constructor(private menu: MenuController) { }
+  constructor(private menu: MenuController,
+    private authService: AuthService,
+    private afs: AngularFirestore,
+    private  modalController: ModalController) { 
+      this.authService.getAuth().onAuthStateChanged(user => {
+        if (user) {
+          this.afs.collection('User')
+            .doc(user.uid)
+            .valueChanges()
+            .subscribe(docUser => {
+              this.savemoney(docUser)
+            });
+          }
+        });
+    }
 
+  ngOnInit() {
+  }
+  
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
@@ -22,6 +46,9 @@ export class HomePage {
   openCustom() {
     this.menu.enable(true, 'custom');
     this.menu.open('custom');
+  }
+  savemoney(doc) {
+    this.dataUser = doc;
   }
 
 }
